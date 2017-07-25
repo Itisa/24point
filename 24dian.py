8288995e0892bfecf28ab456f81b33ac3eaaf307
Itisa <shshr2005@163.com>
2017-07-25 11:27:09 +0800

    24point
    done

--- 24dian.py
+++ 24dian.py
@@ -1,6 +1,7 @@
 import sys
 
 def points(a,b,c,d):
+	ans3 = 0
 	p1 = 0
 	p2 = 0
 	for z in range(24):
@@ -98,24 +99,59 @@ def points(a,b,c,d):
 		elif p1 >18 and p1 < 21:
 			c1 = a
 			d1 = b
-			if p1 == 16:
+			if p1 == 18:
 				a1 = c
 				b1 = d
 			else:
 				a1 = d
 				b1 = c
-		elif p1 >20 and p1 < 19:
+		elif p1 >20 and p1 < 23:
 			d1 = a
 			a1 = b
-			if p1 == 16:
+			if p1 == 20:
 				b1 = c
 				c1 = d
 			else:
 				b1 = d
 				c1 = c
-
-
-			
+		elif p1 >22 and p1 < 25:
+			a1 = b
+			c1 = a
+			if p1 == 22:
+				b1 = c
+				d1 = d
+			else:
+				b1 = d
+				d1 = c
+		elif p1 >24 and p1 < 27:
+			b1 = b
+			d1 = a
+			if p1 == 24:
+				c1 = c
+				a1 = d
+			else:
+				c1 = d
+				a1 = c
+		elif p1 >26 and p1 < 29:
+			c1 = b
+			a1 = a
+			if p1 ==26:
+				d1 = c
+				b1 = d
+			else:
+				d1 = d
+				b1 = c
+		else:
+			d1 = b
+			b1 = a
+			if p1 == 28:
+				a1 = c
+				c1 = d
+			else:
+				a1 = d
+				c1 = c
+		if ans3 == 24:
+			break
 		a2 = 0
 		for z1 in range(96):
 			a2 += 1
@@ -158,12 +194,10 @@ def points(a,b,c,d):
 				ans3 = ans2 / d1
 				fu3 = '/'
 				
-				if ans3 == 24:
-					break				
-					s = [a1,fu1,b1,fu2,c1,fu3,d1]
-					for z in s:
-						print z,
-				elif fu1 == '/' and fu2 == '/' and fu3 =='/' and p1 == 24:
-					print 'nothing'
-
-points(1,2,3,4)
+			if ans3 == 24:								
+				s = [a1,fu1,b1,fu2,c1,fu3,d1]
+				for z in s:
+					print z, 
+				print '= 24'
+				break
+points(10,10,4,4)
